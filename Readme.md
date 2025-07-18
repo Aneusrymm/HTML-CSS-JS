26cd0429a9fb51cd4081a85ca6fd711b291665d0
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-07-18 10:01:13 +0700

    Init gh-pages branch

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Link Webiste :
+File Sosmed : 
---
+++ asycronus/index.js
@@ -0,0 +1,17 @@
+function satu (x,y){
+    h = x+y
+   return h;
+}
+
+function dua (){
+
+}
+
+function tiga (){
+    console.log('ini tiga')
+}
+
+
+console.log(satu(1,2))
+
+
