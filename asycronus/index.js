63f512ca0ccedfef38f752e0f2624f4abdcd31f6
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-07-24 08:37:14 +0700

    update

--- asycronus/index.js
+++ asycronus/index.js
@@ -1,17 +1,26 @@
-function satu (x,y){
-    h = x+y
-   return h;
+async function getUser(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+    const data = await response.json();
+    console.log(data.address.street);
 }
 
-function dua (){
+getUser();
 
+async function showDogImage() {
+  const res = await fetch('https://dog.ceo/api/breeds/image/random');
+  const data = await res.json();
+  const img = document.createElement("img");
+  img.src = data.message;
+  img.width = 300;
+  document.body.appendChild(img);
 }
 
-function tiga (){
-    console.log('ini tiga')
-}
+showDogImage();
 
-
-console.log(satu(1,2))
+ const kotak = document.getElementById("kotak");
+  console.log(kotak.children);        // Semua anaknya (NodeList)
+  console.log(kotak.firstElementChild); // Anak pertama
+  console.log(kotak.lastElementChild);  // Anak terakhir
+  console.log(kotak.parentElement);   // Elemen induk dari kotak
 
 
