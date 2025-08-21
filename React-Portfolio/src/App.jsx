66e1d726340c029b614770e0dd610ce583996523
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-08-21 09:41:59 +0700

    Update

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DataImage from "./data"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 gird-cols-1">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+          <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"/>
+          <p>Kode yang indah datang dari ketekunan.😊</p>
+        </div>
+        <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Nabil Firdaus</h1>
+        <p className="text-base/loose mb-6 opacity-50">
+          Saya mempunyai ketertarikan dalam bidang Programming dan Data Science,
+          terutama pada pembuatan Fullstack Website , Data Visualization maupun Data Forcasting
+          ketertarikan pada bidang ini sudah berlangsung lebih dari 3 Tahun untuk semua Bidang.
         </p>
+        <div className="flex items-center sm:gap-4 gap-2">
+          <a href="#" className="bg-violet-700 p-4 rounded-4xl hover:bg-violet-600">
+            Download CV <i className="ri-download-line ri-lg"></i>
+          </a>
+          <a href="#" className="bg-zinc-700 p-4 rounded-4xl hover:bg-zinc-600">
+            Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i>
+            </a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto"/>
+    </div>
     </>
   )
 }
--- React-Portfolio/src/main.jsx
+++ React-Portfolio/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import Navbar from './components/Navbar.jsx'
+import 'remixicon/fonts/remixicon.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className='Container mx-auto px-10'>
+    <Navbar />
     <App />
+    </div>
   </StrictMode>,
 )
