6f6a1d2476cc01e0806798edaac5f5cceb8322c9
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-08-21 11:45:34 +0700

    Update "Tentang"

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import DataImage from "./data"
-
+import { listTools } from "./data"
 function App() {
   
 
@@ -27,6 +27,54 @@ function App() {
         </div>
       </div>
       <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto"/>
+    </div>
+
+    {/* Tentang */}
+    <div className="tentang mt-32 py-10">
+      <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+        <img src={DataImage.HeroImage} alt="Tentang Image" className="w-12 rounded-md mb-10 sm:hidden"/>
+        <p className="text-base/loose mb-10">
+          Hi, perkenalkan saya Diky Prayoga, 
+          seorang Full Stack Web Developer dan Designer untuk UI/UX Design maupun Product Digital, 
+          Saya percaya bahwa desain dan fungsionalitas harus berjalan beriringan, 
+          sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi juga memberikan pengalaman pengguna yang optimal.
+        </p>
+        <div className="flex items-center justify-between">
+          <img src={DataImage.HeroImage} alt="Tentang Image" className="w-12 rounded-md sm:block hidden"/>
+          <div className="flex items-center gap-6">
+            <div>
+              <h1 className="text-4xl mb-1">45
+                <span className="text-violet-500">+</span>
+              </h1>
+              <p>Project Selesai</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h1 className="text-4xl mb-1">3
+                <span className="text-violet-500">+</span>
+              </h1>
+              <p>Tahun Pengelaman</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="tools mt-32">
+        <h1 className="text-4xl/snug font-bold mb-4">Tools yang Saya Gunakan</h1>
+        <p className="w-2/5 text-base/loose opacity-50">Berikut adalah beberapa tools yang saya gunakan untuk membuat proyek-proyek saya</p>
+        <div className="tools-box mt-14 grid grid-cols-4 gap-4">
+          {listTools.map(tool =>(
+           <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
+              <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 hover:bg-zinc-900"/>
+              <div>
+                <h4 className="font-bold">{tool.nama}</h4>
+                <p className="opacity-50">{tool.ket}</p>
+              </div>
+           </div>  
+          ))}
+        </div>
+      </div>
     </div>
     </>
   )
