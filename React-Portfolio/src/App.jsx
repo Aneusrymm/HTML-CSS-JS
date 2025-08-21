c1595a10c4aa60516e6835aa3e85b750f854af64
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-08-21 15:53:44 +0700

    Update

--- React-Portfolio/src/App.jsx
+++ React-Portfolio/src/App.jsx
@@ -1,5 +1,5 @@
-import DataImage from "./data"
-import { listTools } from "./data"
+import DataImage, { listProyek } from "./data"
+import { listTools} from "./data"
 function App() {
   
 
@@ -8,7 +8,7 @@ function App() {
     <div className="hero grid md:grid-cols-2 items-center pt-10 xl:gap-0 gap-6 gird-cols-1">
       <div>
         <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
-          <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"/>
+          <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy"/>
           <p>Kode yang indah datang dari ketekunan.😊</p>
         </div>
         <h1 className="text-5xl/tight font-bold mb-6">Hi, Saya Nabil Firdaus</h1>
@@ -26,18 +26,18 @@ function App() {
             </a>
         </div>
       </div>
-      <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto"/>
+      <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] md:ml-auto" loading="lazy"/>
     </div>
 
     {/* Tentang */}
     <div className="tentang mt-32 py-10">
       <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
-        <img src={DataImage.HeroImage} alt="Tentang Image" className="w-12 rounded-md mb-10 sm:hidden"/>
+        <img src={DataImage.HeroImage} alt="Tentang Image" className="w-12 rounded-md mb-10 sm:hidden" loading="lazy"/>
         <p className="text-base/loose mb-10">
-          Hi, perkenalkan saya Diky Prayoga, 
-          seorang Full Stack Web Developer dan Designer untuk UI/UX Design maupun Product Digital, 
-          Saya percaya bahwa desain dan fungsionalitas harus berjalan beriringan, 
-          sehingga setiap proyek yang saya kembangkan tidak hanya terlihat menarik tetapi juga memberikan pengalaman pengguna yang optimal.
+          Hi, perkenalkan saya Muhamad Nabil Firdaus<br></br>
+          seorang Programmer dan Data Enthausiast, 
+          Saya meyakini bahwa penguasaan data dan pemrograman harus berjalan seimbang, 
+          sehingga setiap solusi yang saya kembangkan tidak hanya memiliki logika yang kuat dan efisien, tetapi juga mampu memberikan wawasan yang bernilai serta dapat diimplementasikan secara nyata pada berbagai kebutuhan.
         </p>
         <div className="flex items-center justify-between">
           <img src={DataImage.HeroImage} alt="Tentang Image" className="w-12 rounded-md sm:block hidden"/>
@@ -62,8 +62,8 @@ function App() {
 
       <div className="tools mt-32">
         <h1 className="text-4xl/snug font-bold mb-4">Tools yang Saya Gunakan</h1>
-        <p className="w-2/5 text-base/loose opacity-50">Berikut adalah beberapa tools yang saya gunakan untuk membuat proyek-proyek saya</p>
-        <div className="tools-box mt-14 grid grid-cols-4 gap-4">
+        <p className="xl:w-2/5 lg:2/4 text-base/loose opacity-50">Berikut adalah beberapa tools yang saya gunakan untuk membuat proyek-proyek saya</p>
+        <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
           {listTools.map(tool =>(
            <div className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group" key={tool.id}>
               <img src={tool.gambar} alt="Tools Image" className="w-14 bg-zinc-800 p-1 hover:bg-zinc-900"/>
@@ -76,6 +76,64 @@ function App() {
         </div>
       </div>
     </div>
+    {/* Tentang */}
+
+
+    {/* Project */}
+          <div className="proyek mt-32 py-10">
+            <h1 className="text-4xl font-bold mb-2 text-center">Project</h1>
+              <p className="text-base/loose opacity-50 text-center">
+                Berikut Adalah beberapa proyek yang telah saya kerjakan
+              </p>
+              <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+                {listProyek.map(proyek => (
+                  <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+                    <img src={proyek.gambar} alt="Project Image" loading="lazy"/>
+                    <h1 className="text2xl font-bold my-4">{proyek.nama}</h1>
+                    <p className="text-base/loose mb-4">{proyek.desk}</p>
+                    <div className="flex flex-wrap gap-2">
+                      {proyek.tools.map((tool, index) => (
+                        <p className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold" key={index}>{tool}</p>
+                      ))}
+                    </div>  
+                    <div className="mt-8 text-center">
+                      <a href="#" className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600">
+                        Lihat Website
+                      </a>
+                    </div>  
+                  </div>
+                ))}
+            </div>
+          </div>
+    {/* Project */}
+
+    {/* Kontak */}
+          <div className="kontak mt-32 p-10">
+            <h1 className="text-4xl mb-2 font-bold text-center">Kontak</h1>
+            <p className="text-base/loose mb-10 text-center opacity-50">Mari Terhubung Dengan saya.</p>
+            <form action="" className="bg-zinc-800 p-10 w-fit mx-auto rounded-md" autoComplete="off">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                  <label className="font-semibold">Nama Lengkap</label>
+                  <input type="text" placeholder="Nama Lengkap..." className="border border-zinc-800 p-2 rounded-md" required/>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="font-semibold">Email</label>
+                  <input type="email" placeholder="Nama Email..." className="border border-zinc-800 p-2 rounded-md" required/>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="font-semibold" htmlFor="pesan">Pesan</label>
+                  <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border border-zinc-800 p-2 rounded-md" required></textarea>
+                </div>
+                <div>
+                  <button type="submit" className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600">
+                    Kirim Pesan
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>      
+    {/* Kontak */}
     </>
   )
 }
