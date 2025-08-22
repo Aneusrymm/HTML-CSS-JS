4b1aadfb9134a15ecd92da298dd5d18a623c592c
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-08-22 08:24:11 +0700

    Update

--- React-Portfolio/vite.config.js
+++ React-Portfolio/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-   base: '/HTML-CSS-JS/React-Portfolio/',
+   base: '/React-Portfolio/',
   
 })
