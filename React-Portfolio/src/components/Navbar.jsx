333141de93d01367e1c37548b9263605153404f9
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-08-25 14:34:32 +0700

    Update

--- React-Portfolio/src/components/Navbar.jsx
+++ React-Portfolio/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
- }, []);
+    }, []);
   return (
     <div className="navbar py-7 flex items-center justify-between">
         <div className="logo">
@@ -24,7 +24,6 @@ const Navbar = () => {
         <ul className={`menu z-40 flex items-center sm:gap-10 gap-4 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all md:transition-none
             ${active ? "top-0 opacity-100" : "-top10 opacity-0" }`
             }>
-
                 <li>
                     <a href="#beranda" className="sm:text-lg text-base font-medium">Beranda</a>
                 </li>
